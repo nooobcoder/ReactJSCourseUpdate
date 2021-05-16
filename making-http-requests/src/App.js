8d5586074f96b7b0090c40057eaeaf27735ed147
nooobcoder <suryashi2013@gmail.com>
2021-05-16 05:15:50 +0000

    Making HTTP Requests module complete

--- making-http-requests/src/App.js
+++ making-http-requests/src/App.js
@@ -1,73 +1,110 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useReducer, useEffect, useCallback } from "react";
+import "./App.css";
 
-import MoviesList from './components/MoviesList';
-import AddMovie from './components/AddMovie';
-import './App.css';
+import AddMovie from "./components/AddMovie";
+import MoviesList from "./components/MoviesList";
 
-function App() {
-  const [movies, setMovies] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
+const App = () => {
+	const apiManagerReducer = (prevState, { type, payload }) => {
+		switch (type) {
+			case "FETCH_MOVIES":
+				return { movies: payload, loading: true };
+			case "SET_LOADING":
+				return { ...prevState, loading: payload };
+			case "RAISE_ERROR":
+				return { ...prevState, loading: false, error: payload };
+			default:
+				return prevState;
+		}
+	};
 
-  const fetchMoviesHandler = useCallback(async () => {
-    setIsLoading(true);
-    setError(null);
-    try {
-      const response = await fetch('https://swapi.dev/api/films/');
-      if (!response.ok) {
-        throw new Error('Something went wrong!');
-      }
+	const initialState = { movies: [], loading: false, error: null };
+	const [{ movies, loading, error }, apiReducer] = useReducer(
+		apiManagerReducer,
+		initialState
+	);
 
-      const data = await response.json();
+	const fetchMoviesHandler = useCallback(async () => {
+		try {
+			const responseStream = await fetch(
+				`${process.env.REACT_APP_API_ENDPOINT}/movies.json`,
+				{
+					method: "GET",
+				}
+			);
+			if (responseStream.status === 404) {
+				throw new Error(
+					`Something went wrong! Error code: ${responseStream.status}`
+				);
+			}
+			const data = await responseStream.json();
+			const transformedMovies = [];
+			for (const [key, value] of Object.entries(data)) {
+				transformedMovies.push({ ...value, id: key });
+			}
 
-      const transformedMovies = data.results.map((movieData) => {
-        return {
-          id: movieData.episode_id,
-          title: movieData.title,
-          openingText: movieData.opening_crawl,
-          releaseDate: movieData.release_date,
-        };
-      });
-      setMovies(transformedMovies);
-    } catch (error) {
-      setError(error.message);
-    }
-    setIsLoading(false);
-  }, []);
+			apiReducer({ type: "FETCH_MOVIES", payload: transformedMovies });
+			setTimeout(() => {
+				// ! Added 1.5 sec delay to fake loading state appearance
+				apiReducer({ type: "SET_LOADING", payload: false }); // STOP loading after movies has been loaded
+			}, 1500);
+		} catch (error) {
+			apiReducer({ type: "RAISE_ERROR", payload: error.message });
+		}
+	}, []);
 
-  useEffect(() => {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
+	useEffect(() => {
+		fetchMoviesHandler();
+	}, [fetchMoviesHandler]);
 
-  function addMovieHandler(movie) {
-    console.log(movie);
-  }
+	const addMovieHandler = async (movie) => {
+		const config = {
+			method: "POST",
+			mode: "cors",
+			cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+			credentials: "same-origin", // include, *same-origin, omit
+			headers: {
+				"Content-Type": "application/json",
+			},
+			referrerPolicy: "no-referrer",
+		};
+		const response = await fetch(
+			`${process.env.REACT_APP_API_ENDPOINT}/movies.json`,
+			{
+				...config,
+				body: JSON.stringify(movie),
+			}
+		);
+		// const data = await response.json();
+	};
 
-  let content = <p>Found no movies.</p>;
-
-  if (movies.length > 0) {
-    content = <MoviesList movies={movies} />;
-  }
-
-  if (error) {
-    content = <p>{error}</p>;
-  }
-
-  if (isLoading) {
-    content = <p>Loading...</p>;
-  }
-
-  return (
-    <React.Fragment>
-      <section>
-        <AddMovie onAddMovie={addMovieHandler} />
-      </section>
-      <section>
-        <button onClick={fetchMoviesHandler}>Fetch Movies</button>
-      </section>
-      <section>{content}</section>
-    </React.Fragment>
-  );
-}
+	return (
+		<React.Fragment>
+			<section>
+				<AddMovie onAddMovie={addMovieHandler} />
+			</section>
+			<section>
+				<button
+					onClick={(event) => {
+						event.preventDefault();
+						fetchMoviesHandler();
+					}}
+				>
+					Fetch Movies
+				</button>
+			</section>
+			<section>
+				{!loading && movies.length >= 1 && !error && (
+					<MoviesList movies={movies} />
+				)}
+				{!error && !loading && movies.length === 0 && (
+					<p>Found no movies!</p>
+				)}
+				{loading && <p>Loading movies!</p>}
+				{error && !loading && <p>{error}</p>}
+			</section>
+		</React.Fragment>
+	);
+};
 
 export default App;
--- making-http-requests/src/components/Movie.js
+++ making-http-requests/src/components/Movie.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 
-import classes from './Movie.module.css';
+import classes from "./Movie.module.css";
 
-const Movie = (props) => {
-  return (
-    <li className={classes.movie}>
-      <h2>{props.title}</h2>
-      <h3>{props.releaseDate}</h3>
-      <p>{props.openingText}</p>
-    </li>
-  );
+const Movie = ({ title, releaseDate, openingText }) => {
+	return (
+		<li className={classes.movie}>
+			<h2>{title}</h2>
+			<h3>{releaseDate}</h3>
+			<p>{openingText}</p>
+		</li>
+	);
 };
 
 export default Movie;
