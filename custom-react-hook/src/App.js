409acc5a026f101de6dde487d1bde05a3fff02e4
nooobcoder <suryashi2013@gmail.com>
2021-05-16 13:40:58 +0000

    Custom React Hooks complete

--- custom-react-hook/src/App.js
+++ custom-react-hook/src/App.js
@@ -1,14 +1,54 @@
-import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import React, { useEffect, useState } from "react";
 
-function App() {
-  return (
-    <React.Fragment>
-      <ForwardCounter />
-      <BackwardCounter />
-    </React.Fragment>
-  );
-}
+import useHttp from "./hooks/useHttp";
+
+import Tasks from "./components/Tasks/Tasks";
+import NewTask from "./components/NewTask/NewTask";
+
+const App = () => {
+	const [tasks, setTasks] = useState([]);
+
+	const transformTasks = (tasksObj) => {
+		const loadedTasks = [];
+
+		for (const taskKey in tasksObj) {
+			loadedTasks.push({ id: taskKey, text: tasksObj[taskKey].text });
+		}
+
+		setTasks(loadedTasks);
+	};
+
+	const {
+		isLoading,
+		error,
+		sendRequest: fetchTasks,
+	} = useHttp(
+		{
+			url: `${process.env.REACT_APP_API_ENDPOINT}/tasks.json`,
+			method: "GET",
+		},
+		transformTasks
+	);
+
+	useEffect(() => {
+		fetchTasks();
+	}, []);
+
+	const taskAddHandler = (task) => {
+		setTasks((prevTasks) => prevTasks.concat(task));
+	};
+
+	return (
+		<React.Fragment>
+			<NewTask onAddTask={taskAddHandler} />
+			<Tasks
+				items={tasks}
+				loading={isLoading}
+				error={error}
+				onFetch={fetchTasks}
+			/>
+		</React.Fragment>
+	);
+};
 
 export default App;
