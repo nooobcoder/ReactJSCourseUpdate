4e764260e672a46c2b7e1f43badeec47fd8f729b
nooobcoder <suryashi2013@gmail.com>
2021-05-21 05:38:46 +0000

    Added HTTP to the app

--- food-order-app/src/components/Cart/Cart.js
+++ food-order-app/src/components/Cart/Cart.js
@@ -1,51 +1,115 @@
 import styles from "../../styles/Cart/Cart.module.css";
-import Modal from "../UI/Modal"
+import Modal from "../UI/Modal";
+import Checkout from "./Checkout";
 
-import React, {useContext} from 'react';
-import {CartContext} from "../../store/CartContext";
+import React, { useContext, useState } from "react";
+import axios from "axios";
+import { CartContext } from "../../store/CartContext";
 import CartItem from "./CartItem";
 
+const Cart = ({ onClose }) => {
+	const [isCheckout, setIsCheckout] = useState(false);
+	const [isSubmitting, setIsSubmitting] = useState(false);
+	const [didSubmit, setDidSubmit] = useState(false);
 
-const Cart = ({onClose}) => {
-  const cartContextData = useContext(CartContext);
+	const cartContextData = useContext(CartContext);
 
-  const totalAmount = `$${cartContextData.totalAmount.toFixed(2)}`;
-  const hasItems = cartContextData.items.length > 0;
+	const totalAmount = `$${cartContextData.totalAmount.toFixed(2)}`;
+	const hasItems = cartContextData.items.length > 0;
 
-  const cartItemRemoveHandler = (id) => {
-    cartContextData.removeItem(id);
-  }
-  const cartItemAddHandler = (item) => {
-    cartContextData.addItem(item);
-  };
+	const cartItemRemoveHandler = (id) => {
+		cartContextData.removeItem(id);
+	};
+	const cartItemAddHandler = (item) => {
+		cartContextData.addItem(item);
+	};
 
+	const cartItems = (
+		<ul className={styles["cart-items"]}>
+			{cartContextData.items.map((item) => (
+				<CartItem
+					key={item.id}
+					name={item.name}
+					amount={item.amount}
+					price={item.price}
+					onRemove={cartItemRemoveHandler.bind(null, item.id)}
+					onAdd={cartItemAddHandler.bind(null, item)}
+				/>
+			))}
+		</ul>
+	);
 
-  const cartItems = <ul className={styles['cart-items']}>
-    {cartContextData.items.map((item) => (
-      <CartItem
-        key={item.id}
-        name={item.name}
-        amount={item.amount}
-        price={item.price}
-        onRemove={cartItemRemoveHandler.bind(null, item.id)}
-        onAdd={cartItemAddHandler.bind(null, item)}
-      />
-    ))}
-  </ul>
+	const orderHandler = () => {
+		setIsCheckout(true);
+	};
 
-  return (
-    <Modal onClose={onClose}>
-      {cartItems}
-      <div className={styles.total}>
-        <span>Total Amount</span>
-        <span>{totalAmount}</span>
-      </div>
-      <div className={styles.actions}>
-        <button className={styles['button--alt']} onClick={onClose}>CLOSE</button>
-        {hasItems && <button className={styles.button}>Order</button>}
-      </div>
-    </Modal>
-  );
+	const modalActions = (
+		<div className={styles.actions}>
+			<button className={styles["button--alt"]} onClick={onClose}>
+				CLOSE
+			</button>
+			{hasItems && (
+				<button
+					className={styles.button}
+					onClick={() => {
+						orderHandler();
+					}}
+				>
+					Order
+				</button>
+			)}
+		</div>
+	);
+
+	const submitOrderHandler = async (userData) => {
+		try {
+			setIsSubmitting(true);
+			const response = await axios.post(
+				`${process.env.REACT_APP_API_ENDPOINT}/orders.json`,
+				{ user: userData, orderedItems: cartContextData.items }
+			);
+			setIsSubmitting(false);
+			setDidSubmit(true);
+		} catch (error) {
+			console.clear();
+			console.error("SOME ERROR OCCURED");
+		}
+	};
+
+	const cartModalContent = (
+		<React.Fragment>
+			{" "}
+			{cartItems}
+			<div className={styles.total}>
+				<span>Total Amount</span>
+				<span>{totalAmount}</span>
+			</div>
+			{isCheckout && (
+				<Checkout onConfirm={submitOrderHandler} onCancel={onClose} />
+			)}
+			{!isCheckout && modalActions}
+		</React.Fragment>
+	);
+
+	const isSubmittingModalContent = <p>Sending order data...</p>;
+	const didSubmitModalContent = (
+		<React.Fragment>
+			<p>Successfully sent the order!</p>
+			<div className={styles.actions}>
+				<button className={styles.button} onClick={onClose}>
+					Close
+				</button>
+			</div>
+		</React.Fragment>
+	);
+
+	return (
+		<Modal onClose={onClose}>
+			{!isSubmitting && !didSubmit && cartModalContent}
+			{isSubmitting && isSubmittingModalContent}
+			{!isSubmitting && didSubmit && didSubmitModalContent}
+		</Modal>
+	);
 };
 
 export default Cart;
--- food-order-app/src/components/Meals/AvailableMeals.js
+++ food-order-app/src/components/Meals/AvailableMeals.js
@@ -1,23 +1,65 @@
-import {DUMMY_MEALS} from "./dummy_meals";
-import styles from "../../styles/Meals/AvailableMeals.module.css"
-import React from 'react';
+// import { DUMMY_MEALS } from "./dummy_meals";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import styles from "../../styles/Meals/AvailableMeals.module.css";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
+import Spinner from "../UI/Spinner";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map(({id, name, description, price}) => {
-    return (<MealItem key={id} id={id} name={name} description={description} price={price}/>)
-  })
+	const [meals, setMeals] = useState([]);
+	const [isLoading, setIsLoading] = useState(false);
+	const [httpError, setHttpError] = useState(undefined);
 
-  return (
-    <section className={styles.meals}>
-      <Card>
-        <ul>
-          {mealsList}
-        </ul>
-      </Card>
-    </section>
-  );
+	useEffect(() => {
+		const fetchMeals = async () => {
+			try {
+				setIsLoading(true);
+				const response = await axios.get(
+					`${process.env.REACT_APP_API_ENDPOINT}/meals.json`
+				);
+				const meals = [];
+				for (const key in response.data) meals.push(response.data[key]);
+				setMeals((prevState) => meals);
+				setIsLoading(false);
+			} catch (error) {
+				setIsLoading(false);
+				setHttpError({ message: "Database fetch error." });
+				console.clear();
+				console.error("[ ERROR ]", "Database fetch error.");
+			}
+		};
+		fetchMeals();
+	}, []);
+
+	// REACT_APP_API_ENDPOINT
+	// const mealsList = DUMMY_MEALS.map(({ id, name, description, price }) => {
+
+	const mealsList = meals.map(({ id, name, description, price }) => {
+		return (
+			<MealItem
+				key={id}
+				id={id}
+				name={name}
+				description={description}
+				price={price}
+			/>
+		);
+	});
+
+	if (httpError) {
+		return <p className={styles.MealsError}>{httpError.message}</p>;
+	}
+
+	return !isLoading ? (
+		mealsList.length > 0 && (
+			<section className={styles.meals}>
+				<Card>{mealsList}</Card>
+			</section>
+		)
+	) : (
+		<Spinner />
+	);
 };
 
 export default AvailableMeals;
