7dccc756f609d814928b6330d0dbf18f09c6b162
nooobcoder <suryashi2013@gmail.com>
2021-05-27 04:36:32 +0000

    Added Lazy Loading to React App

--- multipage-spa-react-router/project-one/src/App.js
+++ multipage-spa-react-router/project-one/src/App.js
@@ -1,10 +1,12 @@
-import { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useState, Suspense } from "react";
 import { Switch, Route, Redirect } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes";
-import QuoteDetail from "./pages/QuoteDetail";
-import NewQuote from "./pages/NewQuote";
 import Layout from "./components/layout/Layout";
 import NotFound from "./pages/NotFound";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const QuoteDetail = React.lazy(() => import("./pages/QuoteDetail"));
 
 const App = () => {
 	const [quotes, setQuotes] = useState([]);
@@ -38,21 +40,29 @@ const App = () => {
 
 	return (
 		<Layout>
-			<Switch>
-				<Route path="/" exact>
-					<Redirect to="/quotes" />
-				</Route>
-				<Route path="/quotes" exact>
-					<AllQuotes quotes={quotes} />
-				</Route>
-				<Route path="/quotes/:quoteId">
-					<QuoteDetail quotes={quotes} />
-				</Route>
-				<Route path="/new-quote" exact>
-					<NewQuote />
-				</Route>
-				<Route path="*" component={NotFound} />
-			</Switch>
+			<Suspense
+				fallback={
+					<div className="centered">
+						<LoadingSpinner />
+					</div>
+				}
+			>
+				<Switch>
+					<Route path="/" exact>
+						<Redirect to="/quotes" />
+					</Route>
+					<Route path="/quotes" exact>
+						<AllQuotes quotes={quotes} />
+					</Route>
+					<Route path="/quotes/:quoteId">
+						<QuoteDetail quotes={quotes} />
+					</Route>
+					<Route path="/new-quote" exact>
+						<NewQuote />
+					</Route>
+					<Route path="*" component={NotFound} />
+				</Switch>
+			</Suspense>
 		</Layout>
 	);
 };
