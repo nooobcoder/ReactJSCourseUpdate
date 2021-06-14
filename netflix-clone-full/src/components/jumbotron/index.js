e6766b603276ae423cb212bb8235ed09327e8af5
nooobcoder <suryashi2013@gmail.com>
2021-06-14 14:48:24 +0000

    Buiilt the Jumbotron and the container to hold it.

---
+++ netflix-clone-full/src/components/jumbotron/index.js
@@ -0,0 +1,37 @@
+import {
+	Inner,
+	Pane,
+	Title,
+	SubTitle,
+	Image,
+	Item,
+	Container,
+} from "./styles/jumbotron";
+
+const Jumbotron = ({ children, direction = "row", ...restProps }) => (
+	<Item {...restProps}>
+		<Inner direction={direction}>{children}</Inner>
+	</Item>
+);
+
+Jumbotron.Container = ({ children, ...restProps }) => {
+	return <Container {...restProps}>{children}</Container>;
+};
+
+Jumbotron.Pane = ({ children, ...restProps }) => {
+	return <Pane {...restProps}>{children}</Pane>;
+};
+
+Jumbotron.Title = ({ children, ...restProps }) => {
+	return <Title {...restProps}>{children}</Title>;
+};
+
+Jumbotron.SubTitle = ({ children, ...restProps }) => {
+	return <SubTitle {...restProps}>{children}</SubTitle>;
+};
+
+Jumbotron.Image = ({ ...restProps }) => {
+	return <Image {...restProps} />;
+};
+
+export { Jumbotron };
--- netflix-clone-full/src/index.js
+++ netflix-clone-full/src/index.js
@@ -1,14 +1,14 @@
-import React from "react";
+import React, { Fragment } from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import "normalize.css";
+import { GlobalStyles } from "./globalStyles";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
-root.render(<App />);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(
+	<Fragment>
+		<GlobalStyles />
+		<App />
+	</Fragment>
+);
