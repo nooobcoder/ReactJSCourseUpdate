441620dc6c303019091b6147f24764219265df99
nooobcoder <suryashi2013@gmail.com>
2021-06-15 07:30:27 +0000

    Added Header Component and improved routing.

--- netflix-clone-full/src/components/index.js
+++ netflix-clone-full/src/components/index.js
@@ -2,5 +2,7 @@ import Jumbotron from "./jumbotron";
 import Footer from "./footer";
 import Accordion from "./accordion";
 import OptForm from "./opt-form";
+import Header from "./header";
+import Feature from "./feature";
 
-export { Jumbotron, Footer, Accordion, OptForm };
+export { Jumbotron, Footer, Accordion, OptForm, Header, Feature };
--- netflix-clone-full/src/pages/Home.js
+++ netflix-clone-full/src/pages/Home.js
@@ -1,18 +1,38 @@
 import { Fragment } from "react";
+import HeaderContainer from "../containers/HeaderContainer";
 import FaqsContainer from "../containers/faqs";
 import FooterContainer from "../containers/FooterContainer";
 import JumbotronContainer from "../containers/JumbotronContainer";
-import { OptForm } from "../components";
+import { OptForm, Feature } from "../components";
 
 const Home = () => {
-	return (
-		<Fragment>
-			<JumbotronContainer />
-			<FaqsContainer />
-			<OptForm />
-			<FooterContainer />
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      <HeaderContainer>
+        <Feature>
+          <Feature.Title>
+            Unlimited films, TV programmes and more.
+          </Feature.Title>
+          <Feature.SubTitle>
+            Watch anywhere. Cancel at any time.
+          </Feature.SubTitle>
+          <OptForm>
+            <OptForm.Input placeholder="Email address" />
+            <OptForm.Button>Try it now</OptForm.Button>
+            <OptForm.Break />
+            <OptForm.Text>
+              Ready to watch? Enter your email to create or restart your
+              membership.
+            </OptForm.Text>
+          </OptForm>
+        </Feature>
+      </HeaderContainer>
+
+      <JumbotronContainer />
+      <FaqsContainer />
+      <FooterContainer />
+    </Fragment>
+  );
 };
 
 export default Home;
