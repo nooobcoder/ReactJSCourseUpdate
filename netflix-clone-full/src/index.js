786debbd969f6c4b96ed57518b8eb886a296de7a
nooobcoder <suryashi2013@gmail.com>
2021-06-16 03:59:51 +0000

    Set up firebase and provided global context to the app.

--- netflix-clone-full/src/index.js
+++ netflix-clone-full/src/index.js
@@ -3,12 +3,17 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import "normalize.css";
 import { GlobalStyles } from "./globalStyles";
+import { firebaseConnection } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebaseContext";
 
 const container = document.getElementById("root");
 const root = ReactDOM.createRoot(container);
+console.log("Hi");
 root.render(
-		<Fragment>
-			<GlobalStyles />
-			<App />
-		</Fragment>
+  <Fragment>
+    <FirebaseContext.Provider value={firebaseConnection}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
+  </Fragment>
 );
