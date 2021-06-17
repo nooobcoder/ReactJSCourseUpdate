0f26a945e6bb2386080a27afeb75e98ded2ca168
nooobcoder <suryashi2013@gmail.com>
2021-06-17 14:18:24 +0000

    Added custom hook for firebase auth listener.

--- netflix-clone-full/src/App.js
+++ netflix-clone-full/src/App.js
@@ -1,35 +1,18 @@
+import { useSelector } from "react-redux";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { useEffect } from "react";
 import * as ROUTES from "./constants/routes";
-import { Browse, Home, Signin, Signup } from "./pages";
 import {
   IsUserRedirect,
   ProtectedRoute,
 } from "./helpers/routeRedirectorMiddleware";
-import { setAuthState } from "./context/appSlice";
-import { useDispatch, useSelector } from "react-redux";
-import { firebaseConnection } from "./lib/firebase.prod";
+import { Browse, Home, Signin, Signup } from "./pages";
+import { FirebaseAuthStateListener } from "./hooks";
 
 const App = () => {
-  const { firebaseAuthState, isAuthenticated } = useSelector(({ app }) => app);
-  const appStateDispatch = useDispatch();
+  const { firebaseAuthState } = useSelector(({ app }) => app);
   // console.log("Is Authenticated? ", isAuthenticated);
-
-  useEffect(() => {
-    const getLoggedInUser = async () => {
-      firebaseConnection.auth().onAuthStateChanged((user) => {
-        if (user) {
-          appStateDispatch(setAuthState(user));
-        } else {
-          setAuthState({
-            firebaseAuthState: undefined,
-            isAuthenticated: undefined,
-          });
-        }
-      });
-    };
-    getLoggedInUser();
-  }, [appStateDispatch]);
+  FirebaseAuthStateListener();
 
   return (
     <Router>
