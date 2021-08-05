6b4fb33458a92d73fa9acf1f7cbd2334c4203b90
nooobcoder <suryashi2013@gmail.com>
2021-08-05 05:48:23 +0000

    Built banner

---
+++ airbnb-clone/components/Banner.tsx
@@ -0,0 +1,18 @@
+import Image from 'next/image';
+import { FC } from 'react';
+
+const Banner: FC = () => {
+  return (
+    <div className="relative h-[300px] sm:h-[400px] lg:h-[500px] xl:h-[600px] 2xl:h-[700px] font-semibold">
+      <Image src="/banner-bg.webp" layout="fill" objectFit="cover" />
+      <div className="absolute top-1/2 w-full text-center">
+        <p className="text-sm sm:text-lg">Not sure where to go? Perfect.</p>
+        <button className="bg-white text-[#ff5a5f] px-10 py-4 rounded-full font-bold shadow-lg my-3 hover:shadow-xl active:scale-90 transition duration-150">
+          I'm flexible
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Banner;
--- airbnb-clone/pages/index.tsx
+++ airbnb-clone/pages/index.tsx
@@ -1,10 +1,11 @@
 import Head from 'next/head';
 import { FC } from 'react';
+import Banner from '../components/Banner';
 import Header from '../components/Header';
 
 const Home: FC = () => {
   return (
-    <div className="">
+    <div className="font-cereal">
       <Head>
         <title>Airbnb clone - Ankur Paul</title>
         <meta name="description" content="Airbnb clone by Ankur Paul" />
@@ -12,7 +13,7 @@ const Home: FC = () => {
       </Head>
 
       <Header />
-      {/* Banner */}
+      <Banner />
     </div>
   );
 };
