3934b610d1e3979bc235a7a5719a333227b0d668
nooobcoder <suryashi2013@gmail.com>
2021-08-06 16:43:30 +0000

    Designed the date picker with search bar

--- airbnb-clone/components/Header.tsx
+++ airbnb-clone/components/Header.tsx
@@ -1,20 +1,49 @@
 /* PACKAGE IMPORTS */
 import { GlobeAltIcon, MenuIcon, SearchIcon, UserCircleIcon } from '@heroicons/react/solid';
 import Image from 'next/image';
-import React, { FC } from 'react';
+import React, { FC, useState } from 'react';
+import { DateRangePicker, Range } from 'react-date-range';
+import { UsersIcon } from '@heroicons/react/solid';
+// Styled imports for react-date-range
+import 'react-date-range/dist/styles.css'; // main style file
+import 'react-date-range/dist/theme/default.css'; // theme css file
+
 /* --------------- */
 
 const Header: FC = () => {
+  const [searchInput, setSearchInput] = useState<string>('');
+
+  enum DatePickerKeys {
+    selection = 'selection',
+  }
+  const [dateRange, setDateRange] = useState<Range>({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: DatePickerKeys.selection,
+  });
+
+  const handleSelect = (ranges: any) => {
+    setDateRange((prevState) => ({
+      ...prevState,
+      startDate: ranges.selection.startDate,
+      endDate: ranges.selection.endDate,
+    }));
+  };
+  const selectionRange: Range = { ...dateRange };
+
   return (
     <header className="relative top-0 z-30 grid grid-cols-3 shadow-xl py-5 px-5 md:px-10 mx-5 rounded-2xl ">
       <div className="relative flex items-center h-10 cursor-pointer my-auto ">
-        <Image src="/logo.png" layout="fill" objectFit="contain" objectPosition="left" />
+        <Image src="/logo.png" layout="fill" objectFit="contain" objectPosition="left" alt="logo" />
       </div>
 
       <div className="flex items-center md:border-2 rounded-full py-2 md:shadow-md bg-gray-100">
         <input
           type="text"
           placeholder="Start your search"
+          onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+            setSearchInput(event.target.value)
+          }
           className="overflow-auto	 pl-5 bg-transparent flex-grow text-sm text-gray-600 placeholder-gray-300 outline-none"
         />
         <SearchIcon className="hidden h-8 md:inline-flex  bg-red-400 text-white rounded-full p-2 cursor-pointer md:mx-2" />
@@ -28,6 +57,22 @@ const Header: FC = () => {
           <UserCircleIcon className="h-6 cursor-pointer" />
         </div>
       </div>
+
+      {searchInput && (
+        <div className="flex flex-col col-span-3 mx-auto mt-2 drop-shadow-xl ">
+          <DateRangePicker
+            ranges={[selectionRange]}
+            minDate={new Date()}
+            rangeColors={['#FD5B61']}
+            onChange={handleSelect}
+          />
+
+          <div className="flex items-center border-b mb-4 mt-3">
+            <h2 className="text-2xl flex-grow font-semibold ">Number of Guests</h2>
+            <UsersIcon className="h-5" />
+          </div>
+        </div>
+      )}
     </header>
   );
 };
