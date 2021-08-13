137367ab848ce9c56ae51a81d83530500613d062
Ankur Paul <suryashi2013@gmail.com>
2021-08-13 15:52:59 +0000

    Desigedn header component

--- amazon-clone/src/components/Header.tsx
+++ amazon-clone/src/components/Header.tsx
@@ -11,6 +11,8 @@ const Header = () => {
             src={'http://pngimg.com/uploads/amazon/amazon_PNG11.png'}
             width={150}
             height={40}
+            placeholder="blur"
+            blurDataURL="iVBORw0KGgoAAAANSUhEUgAAAJYAAAAoCAQAAAC2yMtsAAAATklEQVR42u3QMQEAAAwCoNm/9Cr4+UAEctSiQJYsWbJkyVIgS5YsWbJkKZAlS5YsWbIUyJIlS5YsWQpkyZIlS5YsBbJkyZIlS5YCWbLWHvNZACkvUXnjAAAAAElFTkSuQmCC"
             objectFit={'contain'}
             className={'cursor-pointer'}
           />
@@ -23,20 +25,45 @@ const Header = () => {
           />
           <SearchIcon className="h-12 p-4" />
         </div>
-        <div className="text-white flex items-center text-xs space-x-6 mx-6 whitespace-nowrap">
-          <div>
+        <div className="text-white flex items-center  text-xs space-x-6 mx-6 whitespace-nowrap">
+          <div className="link ">
+            <img
+              alt="india-flag"
+              className="inline rounded-sm h-6"
+              src="http://purecatamphetamine.github.io/country-flag-icons/3x2/IN.svg"
+            />
+          </div>
+          <div className=" link">
             <p>Hello, Ankur Paul</p>
-            <p>Account &amp; Lists</p>
+            <p className="font-extrabold md:text-sm">Account &amp; Lists</p>
           </div>
-          <div>
+          <div className=" link">
             <p>Returns</p>
-            <p>&amp; Orders</p>
+            <p className="font-extrabold md:text-sm">&amp; Orders</p>
           </div>
-          <div>
-            <ShoppingCartIcon className="h-7" />
-            <p>Cart</p>
+          <div className="relative link flex items-center">
+            <span className="absolute top-0 right-0 md:right-6 z-10 h-4 w-4 bg-yellow-400 text-center rounded-full text-black font-extrabold">
+              {1}
+            </span>
+            <ShoppingCartIcon className="h-7 " />
+            <p className="hidden md:inline font-extrabold md:text-sm mt-4">Cart</p>
           </div>
         </div>
+      </div>
+      <div className="font-ember-italic flex items-center space-x-3 p-2 pl-5 bg-amazon_blue-light text-white text-sm">
+        <p className="flex link items-center">
+          <MenuIcon className="h-6 mr-1" />
+          All
+        </p>
+        <p className="link">Prime Video</p>
+        <p className="link">Amazon Business</p>
+        <p className="link">Today's Deals</p>
+        <p className="link hidden lg:inline-flex">Electronics</p>
+        <p className="link hidden lg:inline-flex">Food &amp; Grocery</p>
+        <p className="link hidden lg:inline-flex">Prime</p>
+        <p className="link hidden lg:inline-flex">Buy Again</p>
+        <p className="link hidden lg:inline-flex">Shopper Toolkit</p>
+        <p className="link hidden lg:inline-flex">Health &amp; Personal Care</p>
       </div>
     </header>
   );
--- amazon-clone/src/pages/index.tsx
+++ amazon-clone/src/pages/index.tsx
@@ -1,13 +1,17 @@
 import Head from 'next/head';
 import Header from '../components/Header';
+import image from '../public/image.jpg';
+import Image from 'next/image';
 
 const Home = () => {
   return (
-    <div>
+    <div className="font-ember">
       <Head>
         <title>Amazon Clone</title>
       </Head>
       <Header />
+
+      <main className="max-w-screen-2xl mx-auto"></main>
     </div>
   );
 };
