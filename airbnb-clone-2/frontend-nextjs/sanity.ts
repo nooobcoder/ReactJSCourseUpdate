eed17606b08c1732362668bbaea57caf4961f645
Ankur Paul <suryashi2013@gmail.com>
2021-09-14 20:00:57 +0000

    Fixed a deprecation warning regarding api version

--- airbnb-clone-2/frontend-nextjs/sanity.ts
+++ airbnb-clone-2/frontend-nextjs/sanity.ts
@@ -14,6 +14,7 @@ const config: any = {
    **/
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  apiVersion: "2021-08-31",
   useCdn: process.env.NODE_ENV === "production",
   /**
    * Set useCdn to `false` if your application require the freshest possible
