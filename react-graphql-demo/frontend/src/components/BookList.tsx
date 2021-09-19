f2fd82327e0e4f4e5c816dd65df70d6921de19b8
nooobcoder <suryashi2013@gmail.com>
2021-09-19 03:53:21 +0000

    Initial commit for frontend

---
+++ react-graphql-demo/frontend/src/components/BookList.tsx
@@ -0,0 +1,13 @@
+const BookList = () => {
+	return (
+		<div>
+			<ul id="book-list">
+				<li>One</li>
+				<li>Two</li>
+      <li>Three</li>
+			</ul>
+		</div>
+	);
+};
+
+export default BookList;
