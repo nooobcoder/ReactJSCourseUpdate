c7dbc5c1262908c7b32d4d659c1ec82fb06087ae
nooobcoder <suryashi2013@gmail.com>
2021-09-19 05:46:38 +0000

    Refactor frontend to query books

--- react-graphql-demo/frontend/src/components/BookList.tsx
+++ react-graphql-demo/frontend/src/components/BookList.tsx
@@ -1,13 +1,55 @@
-const BookList = () => {
+import { gql } from "apollo-boost";
+import { graphql, QueryResult } from "react-apollo";
+import { Key, useEffect } from "react";
+interface AuthorSchema {
+	id: Key;
+	name?: String;
+	age?: Number;
+	books?: Array<BookSchema>;
+}
+
+interface BookSchema {
+	id: Key;
+	name?: String;
+	genre?: String;
+	authorId?: String;
+	author?: Array<AuthorSchema>;
+}
+
+const GET_BOOKS_QUERY = gql`
+	{
+		books {
+			id
+			name
+			genre
+		}
+	}
+`;
+
+interface PropTypes {
+	data: {
+		books: Array<BookSchema>;
+	} & QueryResult;
+}
+
+const BookList: React.FunctionComponent<any> = ({
+	data: { books, loading },
+}: PropTypes): JSX.Element => {
+	useEffect(() => {
+		console.log(books, loading);
+	}, [books, loading]);
+
+	const displayBooks = () =>
+		loading ||
+		books?.map(({ id, name }: BookSchema) => (
+			<li key={id as Key}>{name}</li>
+		));
+
 	return (
 		<div>
-			<ul id="book-list">
-				<li>One</li>
-				<li>Two</li>
-      <li>Three</li>
-			</ul>
+			<ul id="book-list">{displayBooks()}</ul>
 		</div>
 	);
 };
 
-export default BookList;
+export default graphql(GET_BOOKS_QUERY)(BookList);
