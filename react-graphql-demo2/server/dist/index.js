72a6a1af91c7d83ce66906ca5b143fb266820204
nooobcoder <suryashi2013@gmail.com>
2021-09-21 06:40:13 +0000

    Improved resolvers

--- react-graphql-demo2/server/dist/index.js
+++ react-graphql-demo2/server/dist/index.js
@@ -6,12 +6,29 @@ var __makeTemplateObject = (this && this.__makeTemplateObject) || function (cook
 Object.defineProperty(exports, "__esModule", { value: true });
 var apollo_server_1 = require("apollo-server");
 var db_1 = require("./db");
-var typeDefs = (0, apollo_server_1.gql)(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  type MainCard {\n    title: String!\n    image: String!\n  }\n  type Animal {\n    image: String!\n    title: String!\n    rating: Float\n    price: String!\n    slug: String!\n    description: [String!]!\n    stock: Int!\n    onSale: Boolean\n  }\n  type Query {\n    animals: [Animal!]!\n    animal(slug: String!): Animal\n    mainCards: [MainCard]\n  }\n"], ["\n  type MainCard {\n    title: String!\n    image: String!\n  }\n  type Animal {\n    image: String!\n    title: String!\n    rating: Float\n    price: String!\n    slug: String!\n    description: [String!]!\n    stock: Int!\n    onSale: Boolean\n  }\n  type Query {\n    animals: [Animal!]!\n    animal(slug: String!): Animal\n    mainCards: [MainCard]\n  }\n"])));
+var typeDefs = (0, apollo_server_1.gql)(templateObject_1 || (templateObject_1 = __makeTemplateObject(["\n  type MainCard {\n    title: String!\n    image: String!\n  }\n  type Animal {\n    image: String!\n    title: String!\n    rating: Float\n    price: String!\n    slug: String!\n    description: [String!]!\n    stock: Int!\n    onSale: Boolean\n    category: Category\n  }\n  type Category {\n    id: String!\n    image: String!\n    category: String!\n    slug: String!\n    animals: [Animal!]!\n  }\n  type Query {\n    mainCards: [MainCard]\n    animals: [Animal!]!\n    animal(slug: String!): Animal\n    categories: [Category!]!\n    category(slug: String!): Category\n  }\n"], ["\n  type MainCard {\n    title: String!\n    image: String!\n  }\n  type Animal {\n    image: String!\n    title: String!\n    rating: Float\n    price: String!\n    slug: String!\n    description: [String!]!\n    stock: Int!\n    onSale: Boolean\n    category: Category\n  }\n  type Category {\n    id: String!\n    image: String!\n    category: String!\n    slug: String!\n    animals: [Animal!]!\n  }\n  type Query {\n    mainCards: [MainCard]\n    animals: [Animal!]!\n    animal(slug: String!): Animal\n    categories: [Category!]!\n    category(slug: String!): Category\n  }\n"])));
 var resolvers = {
     Query: {
         mainCards: function () { return db_1.mainCards; },
         animals: function () { return db_1.animals; },
         animal: function (parent, args, ctx) { return db_1.animals.find(function (animal) { return animal.slug === args.slug; }); },
+        categories: function () { return db_1.categories; },
+        category: function (_, _a) {
+            var slug = _a.slug;
+            return db_1.categories.find(function (cat) { return cat.slug === slug; });
+        },
+    },
+    Category: {
+        animals: function (_a) {
+            var id = _a.id;
+            return db_1.animals.filter(function (animal) { return animal.category === id; });
+        },
+    },
+    Animal: {
+        category: function (_a) {
+            var category = _a.category;
+            return db_1.categories.find(function (ctgry) { return ctgry.id === category; });
+        },
     },
 };
 var server = new apollo_server_1.ApolloServer({ typeDefs: typeDefs, resolvers: resolvers });
--- react-graphql-demo2/server/src/index.ts
+++ react-graphql-demo2/server/src/index.ts
@@ -1,5 +1,5 @@
 import { ApolloServer, gql } from 'apollo-server';
-import { animals, mainCards } from './db';
+import { animals, categories, mainCards } from './db';
 
 const typeDefs = gql`
   type MainCard {
@@ -15,11 +15,21 @@ const typeDefs = gql`
     description: [String!]!
     stock: Int!
     onSale: Boolean
+    category: Category
+  }
+  type Category {
+    id: String!
+    image: String!
+    category: String!
+    slug: String!
+    animals: [Animal!]!
   }
   type Query {
+    mainCards: [MainCard]
     animals: [Animal!]!
     animal(slug: String!): Animal
-    mainCards: [MainCard]
+    categories: [Category!]!
+    category(slug: String!): Category
   }
 `;
 
@@ -28,6 +38,14 @@ const resolvers = {
     mainCards: () => mainCards,
     animals: () => animals,
     animal: (parent: any, args: any, ctx: any) => animals.find((animal) => animal.slug === args.slug),
+    categories: () => categories,
+    category: (_: any, { slug }: any) => categories.find((cat) => cat.slug === slug),
+  },
+  Category: {
+    animals: ({ id }: any) => animals.filter((animal) => animal.category === id),
+  },
+  Animal: {
+    category: ({ category }: any) => categories.find((ctgry) => ctgry.id === category),
   },
 };
 
