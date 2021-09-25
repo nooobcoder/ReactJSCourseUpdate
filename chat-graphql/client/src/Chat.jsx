decac0e73612af6d1edb0af740141bb88d8e8e2d
nooobcoder <suryashi2013@gmail.com>
2021-09-25 03:56:54 +0000

    Frontend works with polling, websockets to be implemented

--- chat-graphql/client/src/Chat.jsx
+++ chat-graphql/client/src/Chat.jsx
@@ -4,9 +4,11 @@ import {
   ApolloProvider,
   useQuery,
   gql,
+  useMutation,
 } from '@apollo/client';
 
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Container, Row, Col, FormInput, Button } from 'shards-react';
 
 const client = new ApolloClient({
   uri: 'http://192.168.0.120:3000',
@@ -22,17 +24,120 @@ const GET_MESSAGES = gql`
   }
 `;
 
+const POST_MESSAGE = gql`
+  mutation PostMessage($user: String!, $content: String!) {
+    postMessage(user: $user, content: $content)
+  }
+`;
+
 const Messages = ({ user }) => {
-  const { data } = useQuery(GET_MESSAGES);
-  return data
-    ? JSON.stringify(data)
-    : JSON.stringify({ message: 'Invalid Request' });
+  const { data } = useQuery(GET_MESSAGES, { pollInterval: 500 });
+
+  return data ? (
+    <Fragment>
+      {data.messages.map(({ id, user: messageUser, content }) => (
+        <div
+          key={String.toString(id)}
+          style={{
+            display: 'flex',
+            justifyContent: user === messageUser ? 'flex-end' : 'flex-start',
+            paddingBottom: '1em',
+          }}
+        >
+          {user !== messageUser && (
+            <div
+              style={{
+                height: 50,
+                width: 50,
+                marginRight: '0.5em',
+                border: '2px solid #e5e6ea',
+                borderRadius: 25,
+                textAlign: 'center',
+                fontSize: '18pt',
+                paddingTop: 5,
+              }}
+            >
+              {messageUser.slice(0, 2).toUpperCase()}
+            </div>
+          )}
+          <div
+            style={{
+              background: user === messageUser ? 'blue' : '#e5e6ea',
+              color: user === messageUser ? 'white' : 'black',
+              padding: '1em',
+              borderRadius: '1em',
+              maxWidth: '60%',
+            }}
+          >
+            {content}
+          </div>
+        </div>
+      ))}
+    </Fragment>
+  ) : (
+    JSON.stringify({ message: 'Invalid!' })
+  );
 };
 
-const Chat = () => (
+const Chat = () => {
+  const [postMessage] = useMutation(POST_MESSAGE);
+  const onSend = () => {
+    if (state.content.length > 0) {
+      postMessage({ variables: state });
+    }
+    stateSet({ ...state, content: '' });
+  };
+
+  const [state, stateSet] = React.useState({
+    user: 'Ankur Paul',
+    content: 'This is a test message',
+  });
+
+  return (
+    <Container>
+      <Messages user={state.user} />
+      <Row>
+        <Col xs={2} style={{ padding: 0 }}>
+          <FormInput
+            label="User"
+            value={state.user}
+            onChange={(evt) =>
+              stateSet({
+                ...state,
+                user: evt.target.value,
+              })
+            }
+          />
+        </Col>
+        <Col xs={8}>
+          <FormInput
+            label="Content"
+            value={state.content}
+            onChange={(evt) =>
+              stateSet({
+                ...state,
+                content: evt.target.value,
+              })
+            }
+            onKeyUp={(evt) => {
+              if (evt.keyCode === 13) {
+                onSend();
+              }
+            }}
+          />
+        </Col>
+        <Col xs={2} style={{ padding: 0 }}>
+          <Button onClick={() => onSend()} style={{ width: '100%' }}>
+            Send
+          </Button>
+        </Col>
+      </Row>
+    </Container>
+  );
+};
+
+export default () => (
   <ApolloProvider client={client}>
-    <Messages user="Ankur" />
+    <Chat />
   </ApolloProvider>
 );
-
-export default Chat;
