e3ba3400eee982fecb9504f6721b16a4fd1bd426
nooobcoder <suryashi2013@gmail.com>
2021-09-25 03:57:07 +0000

    Prepare backend for subscriptions

--- chat-graphql/server/server.js
+++ chat-graphql/server/server.js
@@ -1,4 +1,4 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 
 const messages = [];
 
@@ -16,7 +16,16 @@ const typeDefs = `
   type Mutation{
     postMessage(user:String!, content:String!):ID!
   }
+
+  type Subscription{
+    messages: [Message!]!
+  }
 `;
+
+const subscribers = [];
+const onMessagesUpdates = (fn) => {
+  subscribers.push(fn);
+};
 
 const resolvers = {
   Query: {
@@ -26,12 +35,24 @@ const resolvers = {
     postMessage: (parent, args, ctx) => {
       const id = messages.length;
       messages.push({ id, ...args });
+      subscribers.forEach((fn) => fn());
       return id;
+    },
+  },
+  Subscription: {
+    messages: {
+      subscribe: (parent, args, { pubsub }) => {
+        const channel = Math.random().toString(36).slice(2, 15);
+        onMessagesUpdates(() => pubsub.publish(channel, { messages }));
+        setTimeout(() => pubsub.publish(channel, { messages }), 0);
+        return pubsub.asyncIterator(channel);
+      },
     },
   },
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const pubsub = new PubSub();
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
 server.start(
   {
     port: 3000,
