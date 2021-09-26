3bd2f33b926f4d9790acc5110e495e7a15109abb
nooobcoder <suryashi2013@gmail.com>
2021-09-26 18:17:02 +0000

    Final fix

--- redux-toolkit/.eslintrc.js
+++ redux-toolkit/.eslintrc.js
@@ -1,27 +1,29 @@
 module.exports = {
-  env: {
-    browser: true,
-    es6: true,
-  },
-  extends: [
-    "plugin:react/recommended",
-    "airbnb",
-    "plugin:prettier/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
-    },
-    ecmaVersion: 12,
-    sourceType: "module",
-  },
-  plugins: ["react", "@typescript-eslint", "unused-imports", "prettier"],
-  rules: {
-    "prettier/prettier": "error",
-    "react/jsx-filename-extension": [
-      2,
-      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
-    ],
-  },
+	env: {
+		browser: true,
+		es2021: true,
+	},
+	extends: [
+		"plugin:react/recommended",
+		"airbnb",
+		"prettier",
+		"plugin:import/errors",
+		"plugin:import/warnings",
+		"plugin:import/typescript",
+	],
+	parser: "@typescript-eslint/parser",
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 12,
+		sourceType: "module",
+	},
+	plugins: ["react", "@typescript-eslint"],
+	rules: {
+		"react/jsx-filename-extension": [0],
+		"import/extensions": "off",
+		"no-use-before-define": "off",
+		"@typescript-eslint/no-use-before-define": ["error"],
+	},
 };
