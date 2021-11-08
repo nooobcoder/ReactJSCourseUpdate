571dd6e8605fef732e5ed24e9b393926bb82804b
nooobcoder <suryashi2013@gmail.com>
2021-11-08 10:33:49 +0530

    Render CommentsForm.jsx and recent comments for each article

--- blog-app-nextjs/components/index.js
+++ blog-app-nextjs/components/index.js
@@ -2,10 +2,22 @@ import Author from './Author';
 import Categories from './Categories';
 import Comments from './Comments';
 import CommentsForm from './CommentsForm';
+import FeaturedPostCard from './FeaturedPostCard';
 import Header from './Header';
 import Layout from './Layout';
 import PostCard from './PostCard';
 import PostDetail from './PostDetail';
 import PostWidget from './PostWidget';
 
-export { Author, Categories, Comments, CommentsForm, Header, Layout, PostCard, PostDetail, PostWidget };
+export {
+  Author,
+  Categories,
+  Comments,
+  CommentsForm,
+  FeaturedPostCard,
+  Header,
+  Layout,
+  PostCard,
+  PostDetail,
+  PostWidget,
+};
