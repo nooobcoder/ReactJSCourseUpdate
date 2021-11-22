0ed17ec5949241302f42a5cd615561ad617536a4
nooobcoder <suryashi2013@gmail.com>
2021-11-22 08:49:51 +0530

    Initial Commit

---
+++ ricky-morty-character-wiki/src/App.js
@@ -0,0 +1,15 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1 className="text-center ubuntu my-4">
+        Rick &amp; Morty <span className="text-primary">Wiki</span>
+      </h1>
+    </div>
+  );
+};
+
+export default App;
