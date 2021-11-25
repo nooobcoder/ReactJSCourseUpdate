aefcdbdb015f77b1e83476bf21318750287156a4
nooobcoder <suryashi2013@gmail.com>
2021-11-26 00:19:50 +0530

    Render blog articles from markdown

--- developer-blog-remix/app/post.ts
+++ developer-blog-remix/app/post.ts
@@ -1,19 +1,48 @@
+import path from "path"
+import fs from "fs/promises";
+import parseFrontMatter, {FrontMatterResult} from "front-matter";
+import invariant from "tiny-invariant"
+import {marked} from "marked";
+
 export type Post = {
-	slug: string;
-	title: string;
+    slug: string;
+    title: string;
+};
+export type PostMarkdownAttributes = {
+    title: string;
 };
 
-export function getPosts() {
-	const posts: Array<Post> = [
-		{
-			slug: "my-first-post",
-			title: "My First Post",
-		},
-		{
-			slug: "90s-mixtape",
-			title: "A Mixtape I Made Just For You",
-		},
-	];
+// Relative to the server path and not src
+let postsPath = path.join(__dirname, "..", "posts")
 
-	return posts;
+// Ensure our posts have the proper meta data and get type safety
+function isValidPostAttributes(attributes: any): attributes is PostMarkdownAttributes {
+    return attributes?.title
+}
+
+export async function getPosts() {
+    let dir = await fs.readdir(postsPath);
+    return Promise.all(
+        dir.map(async filename => {
+            let file = await fs.readFile(path.join(postsPath, filename))
+            let {attributes}: FrontMatterResult<any> = parseFrontMatter(file.toString());
+            invariant(isValidPostAttributes(attributes), `${filename} has bad meta data!`)
+            return {
+                slug: filename.replace(/\.md$/, ""),
+                title: attributes.title
+            }
+        })
+    )
+}
+
+export async function getPost(slug: string) {
+    let filepath = path.join(postsPath, `${slug}.md`)
+    let file = await fs.readFile(filepath);
+    let {attributes, body}: FrontMatterResult<any> = parseFrontMatter(file.toString());
+    invariant(
+        isValidPostAttributes(attributes),
+        `Post ${filepath} is missing attributes`
+    );
+    let html = marked(body)
+    return {slug, html, title: attributes.title};
 }
