2a800670e6859aa0e9151d1dbb9bc3f8a4a9b73b
nooobcoder <suryashi2013@gmail.com>
2021-11-28 12:40:12 +0530

    Mutations of new jokes

--- jokes-app/app/routes/jokes/new.tsx
+++ jokes-app/app/routes/jokes/new.tsx
@@ -1,4 +1,23 @@
 import React from "react";
+import { db } from "../../utils/db.server";
+import { redirect } from "remix";
+import type { ActionFunction } from "remix";
+
+export const action: ActionFunction = async ({ request }) => {
+  const form = await request.formData();
+  const name = form.get("name");
+  const content = form.get("content");
+
+  // we do this type check to be extra sure and to make TypeScript happy
+  // we'll explore validation next!
+  if (typeof name !== "string" || typeof content !== "string") {
+    throw new Error("Form not submitted correctly.");
+  }
+  const fields = { name, content };
+
+  const joke = await db.joke.create({ data: fields });
+  return redirect(`/jokes/${joke.id}`);
+};
 
 export default function NewJokeRoute() {
   return (
