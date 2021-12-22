bd703b3be43827ac347505a7c79fa07400325c36
nooobcoder <suryashi2013@gmail.com>
2021-12-22 19:39:27 +0530

    Barebone routing done

---
+++ remix-blog/app/root.jsx
@@ -0,0 +1,52 @@
+import { Fragment } from "react/cjs/react.production.min";
+import { Outlet, LiveReload, Link, Links, Meta } from "remix";
+import globalStylesURL from "~/styles/global.css";
+
+export const links = () => [{ rel: "stylesheet", href: globalStylesURL }];
+export const meta = () => ({
+	description: "A cool blog built with Remix!",
+	keywords: "remix, react, javascript",
+});
+
+export default () => (
+	<Document>
+		<Layout>
+			<Outlet />
+		</Layout>
+	</Document>
+);
+
+const Document = ({ children, title }) => {
+	return (
+		<html>
+			<head>
+				<Links />
+				<Meta />
+				<title>{title ? title : "My Remix Blog"}</title>
+			</head>
+			<body>
+				{children}
+				{process.env.NODE_ENV === "development" ? <LiveReload /> : null}
+			</body>
+			<h1>My App</h1>
+		</html>
+	);
+};
+
+const Layout = ({ children }) => (
+	<Fragment>
+		<nav className="navbar">
+			<Link to="/" className="remix">
+				Remix
+			</Link>
+
+			<ul className="nav">
+				<li>
+					<Link to="/posts">Posts</Link>
+				</li>
+			</ul>
+		</nav>
+
+		<div>{children}</div>
+	</Fragment>
+);
