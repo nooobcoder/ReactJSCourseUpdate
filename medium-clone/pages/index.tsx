3a18a26ab8ad30dd043044af3c8cbf88400edcbe
nooobcoder <suryashi2013@gmail.com>
2022-01-25 12:50:18 +0530

    IGenerate Posts Card

--- medium-clone/pages/index.tsx
+++ medium-clone/pages/index.tsx
@@ -1,7 +1,8 @@
 import Head from "next/head";
 import Header from "../components/Header";
-import { sanityClient } from "../sanity";
+import { sanityClient, urlFor } from "../sanity";
 import { Post } from "../typings";
+import Link from "next/link";
 interface Props {
 	posts: [Post];
 }
@@ -37,6 +38,39 @@ export default function Home({ posts }: Props) {
 			</div>
 
 			{/* Posts */}
+			<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-3 md:gap-6 p-2 md:p-6">
+				{posts.map((post) => (
+					<Link key={post._id} href={`/post/${post.slug.current}`}>
+						<div className="border rounded-large group cursor-pointer overflow-hidden">
+							{post.mainImage && (
+								<img
+									className="h-60 w-full object-cover group-hover:scale-105 transition-transform duration-200 ease-in-out"
+									src={urlFor(post.mainImage).url() as string}
+								/>
+							)}
+							<div className="flex justify-between p-5 bg-white">
+								<div className="">
+									<p className="text-lg font-bold">
+										{post.title}
+									</p>
+									<p className="text-xs">
+										{post.description} by {post.author.name}
+									</p>
+								</div>
+								<img
+									className="h-12 w-12 rounded-full"
+									src={
+										urlFor(
+											post.author.image
+										).url() as string
+									}
+									alt=""
+								/>
+							</div>
+						</div>
+					</Link>
+				))}
+			</div>
 		</div>
 	);
 }
