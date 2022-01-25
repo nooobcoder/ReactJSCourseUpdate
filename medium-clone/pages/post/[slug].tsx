72ed81dff0b0144aacfe69b2b5761abc55b5db4d
nooobcoder <suryashi2013@gmail.com>
2022-01-25 13:35:08 +0530

    Save Commit

---
+++ medium-clone/pages/post/[slug].tsx
@@ -0,0 +1,119 @@
+// /post/any-url-relative-to-post
+import { sanityClient, urlFor } from "../../sanity";
+import React from "react";
+import Header from "../../components/Header";
+import { Post } from "../../typings";
+import { GetStaticProps } from "next";
+import PortableText from "react-portable-text";
+
+interface Props {
+	post: Post;
+}
+
+const Post = ({ post }: Props) => {
+	console.log(post);
+
+	return (
+		<main>
+			<Header />
+
+			<img
+				src={urlFor(post.mainImage).url() as string}
+				className="w-full h-40 object-cover"
+				alt=""
+			/>
+
+			<article className="max-w-3xl mx-auto p-5">
+				<h1 className="text-3xl mt-10 mb-3">{post.title}</h1>
+				<h2 className="text-xl font-light text-gray-500 mb-2">
+					{post.description}
+				</h2>
+				<div className="flex items-center space-x-2">
+					<img
+						className="h-10 w-10 rounded-full"
+						src={urlFor(post.author.image).url() as string}
+						alt=""
+					/>
+					<p className="font-light text-sm">
+						Blog Post by{" "}
+						<span className="text-green-600">
+							{post.author.name}
+						</span>{" "}
+						- Published at{" "}
+						{new Date(post._createdAt).toLocaleString()}
+					</p>
+				</div>
+
+				<div className="">
+					<PortableText
+						className=""
+						dataset={process.env.NEXT_PUBLIC_SANITY_DATASET!}
+						projectId={process.env.NEXT_PUBLIC_PROJECT_ID!}
+						content={post.body}
+						serializers={{
+							h1: (props: any) => (
+								<h1
+									className="text-2xl font-bold my-5"
+									{...props}
+								/>
+							),
+							h2: (props: any) => (
+								<h1
+									className="text-xl font-bold my-5"
+									{...props}
+								/>
+							),
+							li: ({ children, href }: any) => (
+								<li className="ml-4 list-disc">{children}</li>
+							),
+							link: ({ href, children }: any) => (
+								<a
+									href={href}
+									className="text-blue-500 hover:underline"
+								>
+									{children}
+								</a>
+							),
+						}}
+					/>
+				</div>
+			</article>
+		</main>
+	);
+};
+
+export default Post;
+
+export const getStaticPaths = async () => {
+	const query = `
+                *[_type=="post"] {
+                  _id,
+                  slug {
+                    current
+                  }
+                }
+  `;
+	const posts = await sanityClient.fetch(query);
+
+	const paths = posts.map((post: Post) => ({
+		params: { slug: post.slug.current },
+	}));
+
+	// https://dev.to/tomdohnal/blocking-fallback-for-getstaticpaths-new-next-js-10-feature-1727
+	return { paths, fallback: "blocking" };
+};
+
+// query: *[_type=="post" && slug.current == $slug][0] {
+//   _id,_createdAt,title,author->{name,image},
+// 'comments':*[_type=="comment"&& post._ref==^._id&&approved==true],description,mainImage,slug,body
+// }
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+	const query = `*[_type=="post" && slug.current == $slug][0] {
+  _id,_createdAt,title,author->{name,image},
+'comments':*[_type=="comment"&& post._ref==^._id&&approved==true],description,mainImage,slug,body
+}`;
+
+	const post = await sanityClient.fetch(query, { slug: params?.slug });
+
+	return post ? { props: { post }, revalidate: 60 } : { notFound: true };
+};
