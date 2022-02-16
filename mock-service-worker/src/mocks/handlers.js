f3910f40b6e44f00508e5bf056384e44eaf26eed
nooobcoder <suryashi2013@gmail.com>
2022-02-16 05:26:10 +0000

    Add GitPod init hook

---
+++ mock-service-worker/src/mocks/handlers.js
@@ -0,0 +1,22 @@
+"use strict";
+exports.__esModule = true;
+exports.handlers = void 0;
+var msw_1 = require("msw");
+exports.handlers = [
+    msw_1.rest.post("https://api.medium.com/*", function (req, res, ctx) {
+        return res(ctx.status(201), ctx.json({
+            data: {
+                id: "e6f36a",
+                title: "Liverpool FC",
+                authorId: "5303d74c64f66366f00cb9b2a94f3251bf5",
+                tags: ["football", "sport", "Liverpool"],
+                url: "https://medium.com/@majelbstoat/liverpool-fc-e6f36a",
+                canonicalUrl: "http://jamietalbot.com/posts/liverpool-fc",
+                publishStatus: "public",
+                publishedAt: 1442286338435,
+                license: "all-rights-reserved",
+                licenseUrl: "https://medium.com/policy/9db0094a1e0f"
+            }
+        }));
+    }),
+];
