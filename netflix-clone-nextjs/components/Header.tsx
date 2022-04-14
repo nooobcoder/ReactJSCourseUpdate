437854785a29a8a07a09a2937e728f2857742703
Ankur Paul <suryashi2013@gmail.com>
2022-04-14 21:02:32 +0530

    Day 1 work

--- netflix-clone-nextjs/components/Header.tsx
+++ netflix-clone-nextjs/components/Header.tsx
@@ -3,34 +3,46 @@ import React from 'react'
 import { BellIcon, SearchIcon } from '@heroicons/react/solid'
 
 const Header = () => {
+  const [isScrolled, setIsScrolled] = React.useState(false)
+
+  React.useEffect(() => {
+    const handleScroll = () => {
+      window.scrollY > 0 ? setIsScrolled(true) : setIsScrolled(false)
+    }
+    window.addEventListener('scroll', handleScroll)
+
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
   return (
-    <header>
-      <div className='flex flex-row items-center space-x-2 md:space-x-10'>
+    <header className={`${isScrolled && 'bg-[#141414]'}`}>
+      <div className="flex flex-row items-center space-x-2 md:space-x-10">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src='https://rb.gy/ulxxee'
+          src="https://rb.gy/ulxxee"
           width={100}
           height={100}
-          className='cursor-pointer object-contain'
-          alt={'netflix_logo'} />
-        <ul className='hidden space-x-4 md:flex'>
-          <li className='headerLink'>Home</li>
-          <li className='headerLink'>TV Shows</li>
-          <li className='headerLink'>Movies</li>
-          <li className='headerLink'>New & Popular</li>
-          <li className='headerLink'>My List</li>
+          className="cursor-pointer object-contain"
+          alt={'netflix_logo'}
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
         </ul>
       </div>
-      <div className='flex items-center space-x-4 text-sm font-light'>
-        <SearchIcon className='hidden h-6 w-6 sm:inline' />
-        <p className='hidden lg:inline'>Kids</p>
-        <BellIcon className='h-6 w-6' />
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <SearchIcon className="hidden h-6 w-6 sm:inline" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
         {/* <Link href="/account"> */}
         <img
           onClick={() => null}
-          src='https://rb.gy/g1pwyx'
-          alt=''
-          className='cursor-pointer rounded'
+          src="https://rb.gy/g1pwyx"
+          alt=""
+          className="cursor-pointer rounded"
         />
         {/* </Link> */}
       </div>
---
+++ netflix-clone-nextjs/components/Row.tsx
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Movie } from '../typings'
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/outline'
+import Thumbnail from './Thumbnail'
+
+interface Props {
+  title: string
+  movies: Movie[]
+}
+
+const Row = ({ title, movies }: Props) => {
+  const rowRef = React.useRef<HTMLDivElement>(null)
+  const [isMoved, setIsMoved] = React.useState(false)
+
+  const handleClick = (direction: String) => {
+    setIsMoved(true)
+
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current
+
+      const scrollTo =
+        direction === 'left'
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth
+
+      rowRef.current.scrollTo({ left: scrollTo, behavior: 'smooth' })
+
+      // Check if scrolled to begining
+      if (scrollTo <= 0) {
+        setIsMoved(false)
+      }
+    }
+  }
+
+  return (
+    <div className="h-40 space-y-0.5 md:space-y-2">
+      <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-200 hover:text-white md:text-2xl">
+        {title}
+      </h2>
+      <div className="group relative md:-ml-2">
+        <ChevronLeftIcon
+          className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100 ${
+            !isMoved && 'hidden'
+          }`}
+          onClick={() => handleClick('left')}
+        />
+
+        <div
+          ref={rowRef}
+          className="flex items-center space-x-0.5 overflow-x-scroll scrollbar-hide md:space-x-2.5 md:p-2"
+        >
+          {movies.map((movie) => (
+            <Thumbnail key={movie.id} movie={movie} />
+          ))}
+        </div>
+
+        <ChevronRightIcon
+          className={`absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:scale-125 group-hover:opacity-100`}
+          onClick={() => handleClick('right')}
+        />
+      </div>
+    </div>
+  )
+}
+
+export default Row
