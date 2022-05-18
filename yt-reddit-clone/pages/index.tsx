32604471d3ed1b516db6d03ee3571f1594665acf
nooobcoder <suryashi2013@gmail.com>
2022-05-18 12:40:49 +0000

    Build HEADER and impl next-auth

---
+++ yt-reddit-clone/pages/index.tsx
@@ -0,0 +1,15 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+
+const Home: NextPage = () => {
+  return (
+    // Tailwind space around
+    <div className="">
+      <Head>
+        <title>Reddit 2.0 - nooobcoder</title>
+      </Head>
+    </div>
+  )
+}
+
+export default Home
