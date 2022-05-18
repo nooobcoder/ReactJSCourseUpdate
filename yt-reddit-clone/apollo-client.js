2f4fa8671b879f3017c5da62d08013cb63aa4f2e
nooobcoder <suryashi2013@gmail.com>
2022-05-18 15:28:50 +0000

    Wire up apollo client with frontend

---
+++ yt-reddit-clone/apollo-client.js
@@ -0,0 +1,10 @@
+import { ApolloClient, InMemoryCache } from "@apollo/client"
+
+// Setup apollo client
+export const client = new ApolloClient({
+  uri: 'https://modrany.stepzen.net/api/super-reddit/__graphql',
+  cache: new InMemoryCache(),
+  headers: {
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`
+  }
+});
