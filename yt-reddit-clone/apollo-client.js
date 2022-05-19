ff5bdd3b0d25d1745bffbcc3eec90ae93c565798
nooobcoder <suryashi2013@gmail.com>
2022-05-19 06:43:33 +0000

    stepzen api fixed

--- yt-reddit-clone/apollo-client.js
+++ yt-reddit-clone/apollo-client.js
@@ -5,6 +5,6 @@ export const client = new ApolloClient({
   uri: 'https://modrany.stepzen.net/api/super-reddit/__graphql',
   cache: new InMemoryCache(),
   headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`
+    Authorization: `apikey ${process.env.NEXT_PUBLIC_STEPZEN_API_KEY}`
   }
 });
