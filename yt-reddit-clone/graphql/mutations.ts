eb341105b07dbae9c8a2e262069e87757a886652
nooobcoder <suryashi2013@gmail.com>
2022-05-22 14:55:40 +0000

    Comment component

--- yt-reddit-clone/graphql/mutations.ts
+++ yt-reddit-clone/graphql/mutations.ts
@@ -1,6 +1,6 @@
-import { gql } from '@apollo/client'
+import { gql } from "@apollo/client";
 
-export const ADD_POST = gql`
+const ADD_POST = gql`
   mutation AddPost(
     $body: String!
     $image: String!
@@ -23,9 +23,9 @@ export const ADD_POST = gql`
       subreddit_id
     }
   }
-`
+`;
 
-export const ADD_SUBREDDIT = gql`
+const ADD_SUBREDDIT = gql`
   mutation AddSubreddit($topic: String!) {
     insertSubreddit(topic: $topic) {
       id
@@ -33,4 +33,17 @@ export const ADD_SUBREDDIT = gql`
       created_at
     }
   }
-`
+`;
+
+const ADD_COMMENT = gql`
+  mutation AddComment($post_id: ID!, $username: String!, $text: String!) {
+    insertComment(post_id: $post_id, username: $username, text: $text) {
+      id
+      post_id
+      username
+      text
+    }
+  }
+`;
+
+export { ADD_COMMENT, ADD_SUBREDDIT, ADD_POST };
