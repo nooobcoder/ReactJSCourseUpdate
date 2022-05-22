332b777a8042c1650a84286e15826f8b6a3aaaca
nooobcoder <suryashi2013@gmail.com>
2022-05-22 16:08:45 +0000

    Toasted in render slate

--- yt-reddit-clone/pages/_app.tsx
+++ yt-reddit-clone/pages/_app.tsx
@@ -1,29 +1,29 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { SessionProvider } from 'next-auth/react'
-import Header from '../components/Header'
-import { ApolloProvider } from '@apollo/client'
-import { client } from '../apollo-client'
-import {Toaster} from "react-hot-toast"
+import { ApolloProvider } from "@apollo/client";
+import { SessionProvider } from "next-auth/react";
+import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
+import { client } from "../apollo-client";
+import Header from "../components/Header";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   // Remove all console.logs in production
-  if (process.env.NODE_ENV === 'production') {
-    console.log = () => { }
-    console.info = () => { }
+  if (process.env.NODE_ENV === "production") {
+    console.log = () => {};
+    console.info = () => {};
   }
 
   return (
     <ApolloProvider client={client}>
       <SessionProvider session={session}>
-        <Toaster/>
         <div className="h-screen overflow-y-scroll bg-slate-200">
+          <Toaster />
           <Header />
           <Component {...pageProps} />
         </div>
       </SessionProvider>
     </ApolloProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
