42c6664301e86b2f0132d8e6833f28f8de6262e1
nooobcoder <suryashi2013@gmail.com>
2022-05-26 03:51:01 +0000

    Remix blog tutorial initial commit

---
+++ remix-blog-tutorial/remix.config.js
@@ -0,0 +1,8 @@
+/**
+ * @type {import('@remix-run/dev').AppConfig}
+ */
+module.exports = {
+  cacheDirectory: "./node_modules/.cache/remix",
+  devServerPort: 8002,
+  ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
+};
