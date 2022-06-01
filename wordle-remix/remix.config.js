12df053566c165f9115288a5cbd33025620c5956
nooobcoder <suryashi2013@gmail.com>
2022-06-01 21:08:05 +0530

    Fix netlify deploy for wordle-remix

--- wordle-remix/remix.config.js
+++ wordle-remix/remix.config.js
@@ -2,10 +2,12 @@
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
+	serverBuildTarget: "netlify",
+	server: "./server.js",
 	ignoredRouteFiles: ["**/.*"],
 	devServerPort: 7777,
-	appDirectory: "app",
-	assetsBuildDirectory: "public/build",
-	serverBuildPath: "build/index.js",
-	publicPath: "/build/",
+	// appDirectory: "app",
+	// assetsBuildDirectory: "public/build",
+	// serverBuildPath: ".netlify/functions-internal/server.js",
+	// publicPath: "/build/",
 };
