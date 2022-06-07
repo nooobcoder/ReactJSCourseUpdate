2eaceac961fa2ede59b9dc89709d9919247ca553
Ankur Paul <suryashi2013@gmail.com>
2022-06-07 09:07:19 +0000

    Modify installer script

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -6,8 +6,19 @@ apt-get install -y \
   gnupg2 \
   jq \
   sudo \
-  zsh
+  zsh \
+  ca-certificates \
+  curl \
+  gnupg \
+  lsb-release
 
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
 # set-up and install yarn
 curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
