57c4d29ba3b372403a0e2ee03c12e05bd56f8852
nooobcoder <suryashi2013@gmail.com>
2022-07-01 16:39:03 +0000

    Use tRPC in frontend

--- grocery-list-mern/src/pages/_app.tsx
+++ grocery-list-mern/src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import { AppProps } from 'next/app';
+import { ServerRouter } from '@/server/router';
 import '@/styles/global.css';
+import { AppType } from 'next/dist/shared/lib/utils';
+import { withTRPC } from '@trpc/next';
 
-export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
+const App: AppType = ({ Component, pageProps }) => <Component {...pageProps} />;
+
+export default withTRPC<ServerRouter>({
+  config({ ctx }: any) {
+    const url = process.env.VERCEL_URL
+      ? `https://${process.env.VERCEL_URL}/api/trpc`
+      : `http://localhost:4848/api/trpc`;
+
+    return { url };
+  },
+  ssr: true,
+})(App);
