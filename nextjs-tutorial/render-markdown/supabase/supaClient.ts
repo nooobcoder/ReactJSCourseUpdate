0c2bdc4463920e0040157e9b2f9a16b367a4f121
nooobcoder <suryashi2013@gmail.com>
2022-10-30 13:36:46 +0000

    removed confusing dir

--- nextjs-tutorial/render-markdown/supabase/supaClient.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabase = createClient(
-  'https://qvcrluuvaonuknkyamaz.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYyODg1NzcxMiwiZXhwIjoxOTQ0NDMzNzEyfQ.g3rWM1l6rx5X714ANc6u0QX9wSriSCcxvQ_rI8b4WMo'
-);
-
-export default supabase;
